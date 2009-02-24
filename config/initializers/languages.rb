e08f5756078605ce07f6df10e675bc068f662b91
Kouhei Sutou <kou@clear-code.com>
2009-02-24 16:15:41 +0900

    * add available languages.

---
+++ config/initializers/languages.rb
@@ -0,0 +1,14 @@
+AVAILABLE_LANGUAGES = [
+                       "bn",
+                       "en",
+                       "es",
+                       "id",
+                       "ja",
+                       "ko",
+                       "ms",
+                       "ru",
+                       "th",
+                       "tm",
+                       "vi",
+                       "zh",
+                      ]
