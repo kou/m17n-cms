ac2a14c0f86c5942ffa47f15b58604bfae683803
Kouhei Sutou <kou@clear-code.com>
2009-02-24 15:30:46 +0900

    * children_count isn't needed.

---
+++ db/migrate/20090224062946_remove_children_count_from_page.rb
@@ -0,0 +1,9 @@
+class RemoveChildrenCountFromPage < ActiveRecord::Migration
+  def self.up
+    remove_column :pages, :children_count
+  end
+
+  def self.down
+    add_column :pages, :children_count, :integer
+  end
+end
