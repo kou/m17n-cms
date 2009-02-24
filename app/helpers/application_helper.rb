db368c644dddbc29b125051ebf085cbb633864fb
Kouhei Sutou <kou@clear-code.com>
2009-02-24 14:44:44 +0900

    * use Ext JS.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,54 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def extjs_include_tag
+    tags = [
+            "resources/css/ext-all.css",
+            "adapter/prototype/ext-prototype-adapter.js",
+            "ext-all-debug.js",
+           ].collect do |source|
+      tag = ExtjsAssetTagHelper::ExtjsTag.new(@template, @controller, source)
+      path = tag.public_path
+      case path.sub(/\?.*\z/, '')
+      when /\.css\z/
+        stylesheet_link_tag(path)
+      when /\.js\z/
+        javascript_include_tag(path)
+      else
+        path
+      end
+    end.join("\n")
+  end
+
+  def page_tree_data
+    Page.roots.collect do |page|
+      page_to_tree_data(page)
+    end
+  end
+
+  def page_to_tree_data(page)
+    expanded = false
+    children = page.contents.collect do |content|
+      content_expanded = current_page?(content_path(content))
+      expanded = true if content_expanded
+      {
+        "text" => "#{content.locale}: #{content.title}",
+        "href" => content_path(content),
+        "expanded" => content_expanded,
+        "isLeaf" => true,
+      }
+    end
+    children += page.children.collect do |child|
+      child_tree_data = page_to_tree_data(child)
+      expanded = true if child_tree_data["expanded"]
+      child_tree_data
+    end
+    tree_data = {
+      "text" => page.name,
+      "href" => page_path(page),
+      "children" => children,
+      "isLeaf" => false,
+    }
+    tree_data["expanded"] = true if expanded or current_page?(page_path(page))
+    tree_data
+  end
 end
