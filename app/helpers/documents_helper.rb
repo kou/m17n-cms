f6b58492193504ee18a5472c165504cf79a73b6f
Kouhei Sutou <kou@clear-code.com>
2009-03-02 16:40:59 +0900

    * add usage document.

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,4 +2,8 @@ module DocumentsHelper
   def usage_path
     url_for(:controller => "documents", :action => "usage")
   end
+
+  def usage_for_the_experienced_path
+    url_for(:controller => "documents", :action => "experienced")
+  end
 end
